e0cc4c24ba65abf349386de281f3f46f6cff84df
Andrew-Harsh-CallPotential <aharsh@callpotential.com>
2021-03-08 12:14:14 -0500

    Starter quality check code. Will want to check the percentages later

---
+++ quality_check.bat
@@ -0,0 +1,7 @@
+python -m pylint controllers --fail-under=9 -f colorized
+python -m pylint services --fail-under=9 -f colorized
+python -m pylint models --fail-under=9 -f colorized
+python -m pylint shared_modules --fail-under=9 -f colorized
+python -m coverage run -m unittest discover
+python -m coverage report -m --fail-under 90
+pause
