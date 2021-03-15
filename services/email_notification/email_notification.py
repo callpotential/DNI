f83d99bf370125b45495a48c16c87bfb2a4b9e18
Greg Patterson <gpatterson@callpotential.com>
2021-03-15 17:43:22 -0400

    email notification

---
+++ services/email_notification/email_notification.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+
+def lambda_handler(event, context):
+    print(event)
+
+    SENDER = "dnioverage@callpotential.com"
+    RECIPIENT = "dnioverage@callpotential.com"
+    AWS_REGION = "us-east-2"
+    SUBJECT = event['SUBJECT']
+    BODY_TEXT = event['BODY_TEXT']
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses', region_name=AWS_REGION)
+
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
